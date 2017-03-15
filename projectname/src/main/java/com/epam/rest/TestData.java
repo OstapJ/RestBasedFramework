3161e311150ddb354aa3e37af0b34035fe6fec70
Ievgen Ostapenko <{Ievgen_Ostapenko@epam.com}>
2017-03-15 19:07:22 +0200

    Added testng framework

---
+++ projectname/src/main/java/com/epam/rest/TestData.java
@@ -0,0 +1,15 @@
+package com.epam.rest;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Ievgen_Ostapenko on 3/15/2017.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface TestData {
+    String value();
+}
---
+++ projectname/src/main/java/com/epam/rest/TestDataException.java
@@ -0,0 +1,12 @@
+package com.epam.rest;
+
+public class TestDataException extends RuntimeException {
+
+    public TestDataException(final String message) {
+        super(message);
+    }
+
+    public TestDataException(final String messageTemplate, Object... args) {
+        super(String.format(messageTemplate, args));
+    }
+}
