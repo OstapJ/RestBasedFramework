2788776e935d5e43586f0e972c6a84e4a68b8d48
Ievgen Ostapenko <{Ievgen_Ostapenko@epam.com}>
2017-03-17 18:10:15 +0200

    Fix sonar issues

--- web/src/main/java/step/VideoStep.java
+++ web/src/main/java/step/VideoStep.java
@@ -1,29 +1,28 @@
 package step;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.page;
 
+import com.codeborne.selenide.Configuration;
 import cucumber.api.DataTable;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import page_object.MainPage;
 
 /**
  * Created by Ievgen_Ostapenko on 3/17/2017.
  */
 public class VideoStep {
 
-	MainPage mainPage = page(MainPage.class);
-
-
-	@When("^I navigate to Main page$")
+    @When("^I navigate to Main page$")
     public void navigateTo() {
+		Configuration.browser = "chrome";
         open("http://ecsa00400275.epam.com:4503/content/ellen/en/content.html");
     }
 
     @When("^I search Video with the following values:$")
     public void searchVideoByTags(final DataTable table) {
-		mainPage.getSearchTextBox().setValue(table.getGherkinRows().get(0).getCells().get(0));
+		$("input.search").setValue(table.getGherkinRows().get(1).getCells().get(0));
+
     }
 
     @Then("^I should see the video with the following values:$")
